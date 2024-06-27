67699cd78dbbdc608a0164e312a7f5d613f556a9
Anne <annewangui210@gmail.com>
2024-06-27 15:36:38 +0300

    Adds strings lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+// Define Current User
+const currentUser = `Grace Hopper`;
+// welcome message to contain value stored in current user and end with !
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// excitedWelcomeMessage to contain WELCOME TO FLATBOOK,
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+// shortgreeting to contain initial of currentUser
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
